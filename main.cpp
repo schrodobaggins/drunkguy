6beb678f4665c877d56e8834aab961a5201cd6d6
schrodobaggins <magicalmikely@gmail.com>
2021-03-17 20:43:31 -0700

    changing titles

--- main.cpp
+++ main.cpp
@@ -12,12 +12,12 @@ int main (){
     char x; cin>> x;
     int position = size /2;
     while (true) {
-        cout << "|START|" ;
+        cout << "|STARTING LINE!!!|" ;
         for (int i=0; i<size;i++) {
             if (i == position)
                 cout << x;
             else cout << " ";}
-        cout << "|FINISH|" << endl;
+        cout << "|FINISH LINE!!!|" << endl;
         int move= rand()%3 - 1;
         position = position + move;
         if (position <1) {cout << "You could not finish the race! Not surprising..." <<endl; break;}
